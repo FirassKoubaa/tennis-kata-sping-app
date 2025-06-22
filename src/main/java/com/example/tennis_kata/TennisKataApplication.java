146eac7f04931740f7a7ebfef29352ce47fa2495
KOUBAA Firas <firass.koubaa@gmail.com>
2025-06-22 16:44:52 +0200

    feat: first commit

---
+++ src/main/java/com/example/tennis_kata/TennisKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.tennis_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisKataApplication.class, args);
+	}
+
+}
